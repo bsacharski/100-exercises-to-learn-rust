16e048c7f89fd0b889ee47d213c942e1c20a61f0
Bart& <beton@cementownia.org>
2024-05-29 22:32:00 +0200

    finish chapter 2

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,12 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut acc = 1;
+
+    for i in 2..=n {
+        acc *= i;
+    } 
+
+    return acc;
 }
 
 #[cfg(test)]
