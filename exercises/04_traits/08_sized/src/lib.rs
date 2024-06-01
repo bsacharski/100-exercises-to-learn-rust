0edc425b641556044a1b09ace311726305d62f20
Bart& <beton@cementownia.org>
2024-06-01 11:55:04 +0200

    Implement up to 04/08

--- exercises/04_traits/08_sized/src/lib.rs
+++ exercises/04_traits/08_sized/src/lib.rs
@@ -1,7 +1,18 @@
-pub fn example() {
+pub fn example() -> usize {
     // Trying to get the size of a str (or any other DST)
     // via `std::mem::size_of` will result in a compile-time error.
     //
     // TODO: Comment out the following line and move on to the next exercise.
-    std::mem::size_of::<str>();
+    // ME: but &str does have know size, so let's try it
+    std::mem::size_of::<&str>()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::example;
+
+    #[test]
+    fn size_of_str_ptr() {
+        assert_eq!(example(), 16);
+    }
 }
